92b7a01c9e8ce4340117c0a47178ea8418bb3f7f
Carlo Autor <carloautor@gmail.com>
2023-06-23 15:35:06 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # entrepreneurs-circle-technical-assessment
- entrepreneurs-circle-technical-assessment
+
+entrepreneurs-circle-technical-assessment
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Flexbox
+-   CSS Grid
+-   SCSS
+-   Mobile-first workflow
+
+### Features
+
+-   Added some hover transitions and GSAP animations
+-   Added mobile layout, from 320px up to the largest possible screen
+-   Fully Responsive Website
+
+#### This is built without any framework ✨.
+
+### Links
+
+-   [Live Site URL]()
+
+## Author
+
+-   [Carlo Autor - Github](https://github.com/A14313)
+
+You can support me by donating to my [paypal account](https://www.paypal.me/carloautor). Any amount will be much appreciated :)
---
+++ src/js/animations.js
@@ -0,0 +1,98 @@
+const openBtns = [...document.querySelectorAll('button[data-type="open-modal"]')];
+const closebtn = document.querySelector('button[data-type="close-modal"]');
+const modal = document.querySelector('.modal');
+
+// Hero
+const heroTl = gsap.timeline({
+	defaults: {
+		duration: 1,
+	},
+});
+
+heroTl
+	.fromTo('.hero__left > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 })
+	.fromTo('.person-svg', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' }, '-=.8')
+	.fromTo('.hero__bottom__contents-container > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 })
+	.fromTo('.nav > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 }, '-=.8')
+	.fromTo('.header__bottom', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' }, '-=.8');
+
+// section-two
+const sectionTwoTl = gsap.timeline({
+	defaults: {
+		duration: 1,
+	},
+	scrollTrigger: {
+		trigger: '.section-two',
+		start: 'top 80%',
+		toggleActions: 'play pause resume reverse',
+	},
+});
+sectionTwoTl.fromTo('.section-two__contents-container > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 });
+
+// section-three
+const sectionThreeTl = gsap.timeline({
+	defaults: {
+		duration: 1,
+		ease: 'power4.out',
+	},
+	scrollTrigger: {
+		trigger: '.section-three',
+		start: 'top 80%',
+		toggleActions: 'play pause resume reverse',
+	},
+});
+sectionThreeTl
+	.fromTo('.section-three .section__title', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' })
+	.fromTo('.section-three__list li', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 }, '-=.8')
+	.fromTo('.section-three .btn', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' }, '-=.8');
+
+// section-four
+const sectionFourTl = gsap.timeline({
+	defaults: {
+		duration: 1,
+		ease: 'power4.out',
+	},
+	scrollTrigger: {
+		trigger: '.section-four',
+		start: 'top 80%',
+		toggleActions: 'play pause resume reverse',
+	},
+});
+sectionFourTl
+	.fromTo('.section-four .section__title', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' })
+	.fromTo('.section-four .left', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' }, '-=.8')
+	.fromTo('.section-four .right div > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 }, '-=.8')
+	.fromTo('.section-four .btn', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' }, '-=.4');
+
+// footer
+const footerTl = gsap.timeline({
+	defaults: {
+		duration: 1,
+		ease: 'power4.out',
+	},
+	scrollTrigger: {
+		trigger: '.section-four .btn',
+		start: 'top 80%',
+		endTrigger: '.footer',
+		toggleActions: 'play pause resume reverse',
+	},
+});
+footerTl
+	.fromTo('.footer .company-name', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%' })
+	.fromTo('.footer .socials > *', { autoAlpha: 0, y: '20%' }, { autoAlpha: 1, y: '0%', stagger: 0.3 }, '-=.8');
+// For Modal
+const modalTl = gsap.timeline();
+
+openBtns.forEach((el) => {
+	el.addEventListener('click', () => {
+		if (modalTl.reversed()) {
+			modalTl.play();
+		} else {
+			modalTl.fromTo(modal, { autoAlpha: 0, y: '-100%' }, { autoAlpha: 1, y: '0%', duration: 1 });
+		}
+	});
+});
+
+closebtn.addEventListener('click', () => {
+	modalTl.reverse();
+});
